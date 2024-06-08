7482c8c597d01027806b6aefa3f9b94ea354d0c9
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-06-08 16:24:00 +0700

    adding some firebase configuration and upgrading the dependency for flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:x_buddy/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  getFirebaseInitial();
   runApp(
     GetMaterialApp(
       title: "Application",
@@ -12,4 +16,8 @@ void main() {
       getPages: AppPages.routes,
     ),
   );
+}
+
+void getFirebaseInitial() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
