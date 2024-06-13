c1d87a289effed59bdeccf19b64129ce6722a986
Siswadi24 <danaperdanaputra32@gmail.com>
2024-06-13 18:44:11 +0700

    feat: Implementation of view page creation on appliedEvent

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -24,7 +24,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.APPLIED_EVENT;
 
   static final routes = [
     GetPage(
