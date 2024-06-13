f8ccc6f584831821e65eafa1d8e8ddde3d4d0720
Siswadi24 <danaperdanaputra32@gmail.com>
2024-06-13 20:59:30 +0700

    fixed: Change the main route back to 'HomeRoute'

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -28,7 +28,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.YOUR_EVENT;
+  static const INITIAL = Routes.HOME;
 
   static final routes = [
     GetPage(
