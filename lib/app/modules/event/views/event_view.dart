f75bebe6270cd57944ebebedb27d912d7d4a1500
Siswadi24 <danaperdanaputra32@gmail.com>
2024-06-14 21:22:38 +0700

    fixed: Fixed errors in the searchBar feature

--- lib/app/modules/event/views/event_view.dart
+++ lib/app/modules/event/views/event_view.dart
@@ -10,102 +10,216 @@ class EventView extends GetView<EventController> {
   const EventView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 5),
-              // Search bar
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: SearchBar(
-                  onTap: () {
-                    Get.toNamed(Routes.SEARCH_RESULT);
-                  },
-                  padding: const WidgetStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16)),
-                  hintText: 'Hinted search text',
-                  hintStyle: WidgetStatePropertyAll<TextStyle>(TextStyle(
-                      color: Theme.of(context).colorScheme.onSurfaceVariant,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500)),
-                  backgroundColor: WidgetStatePropertyAll<Color?>(
-                      Theme.of(context)
-                          .colorScheme
-                          .onInverseSurface
-                          .withOpacity(0.8)),
-                  leading: Icon(Icons.search,
-                      color: Theme.of(context).colorScheme.onSurfaceVariant),
-                  onChanged: (value) {},
-                ),
-              ),
-
-              //Feature List scroll horizontal
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          // backgroundColor: Theme.of(context).colorScheme.surface,
+          flexibleSpace: // Search bar
               Container(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      child: Text(
-                        'Feature',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.copyWith(
-                                fontWeight: FontWeight.w600, fontSize: 18),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () => Get.toNamed(Routes.PER_FEATURE_EVENT),
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SearchAnchor(
+                builder: (BuildContext context, SearchController controller) {
+              return SearchBar(
+                hintText: 'Hinted search text',
+                controller: controller,
+                padding: const WidgetStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16.0)),
+                onSubmitted: (value) {
+                  //Called when the user indicates that they are done editing the text in the field.
+                  print('Submitted: $value');
+                  Get.toNamed(Routes.SEARCH_RESULT);
+                },
+                onTap: () {
+                  controller.openView();
+                },
+                onChanged: (_) {
+                  controller.openView();
+                },
+                leading: const Icon(Icons.search),
+              );
+            }, suggestionsBuilder:
+                    (BuildContext context, SearchController controller) {
+              return List<ListTile>.generate(
+                5,
+                (int index) {
+                  final String item = 'item $index';
+                  return ListTile(
+                      title: Text(item),
+                      onTap: () {
+                        // setState(() {
+                        controller.closeView(item);
+                      });
+                },
+              );
+            }),
+          ),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Feature List scroll horizontal
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
                         padding: const EdgeInsets.symmetric(
-                            horizontal: 8.14, vertical: 8.14),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Theme.of(context).cardColor,
-                          border: Border.all(
-                              color:
-                                  Theme.of(context).colorScheme.outlineVariant),
+                            horizontal: 16, vertical: 8),
+                        child: Text(
+                          'Feature',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium
+                              ?.copyWith(
+                                  fontWeight: FontWeight.w600, fontSize: 18),
                         ),
-                        child: Column(
+                      ),
+                      InkWell(
+                        onTap: () => Get.toNamed(Routes.PER_FEATURE_EVENT),
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 16),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.14, vertical: 8.14),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Theme.of(context).cardColor,
+                            border: Border.all(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .outlineVariant),
+                          ),
+                          child: Column(
+                            children: [
+                              SvgPicture.asset('assets/svg/competition.svg'),
+                              Text(
+                                'Competition',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //Populer List Event scroll horizontal
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Text(
+                    'Populer Event',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                ),
+                InkWell(
+                  onTap: () => Get.toNamed(Routes.EVENT_DETAIL),
+                  child: Container(
+                    width: 155,
+                    margin: const EdgeInsets.symmetric(horizontal: 16),
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 0.8,
+                          color: Theme.of(context).colorScheme.outlineVariant,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          "assets/img/event 1.jpeg",
+                          fit: BoxFit.cover,
+                        ),
+                        Wrap(
                           children: [
-                            SvgPicture.asset('assets/svg/competition.svg'),
-                            Text(
-                              'Competition',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge
-                                  ?.copyWith(fontWeight: FontWeight.w600),
+                            Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5),
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 5),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 0.8,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .outline),
+                                  borderRadius: BorderRadius.circular(5)),
+                              child: Text(
+                                'Seminar',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(
+                                        fontWeight: FontWeight.w600,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .outline,
+                                        fontSize: 12),
+                              ),
                             ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 5),
+                              child: Text(
+                                'DevCoach 155 : Data Science | Memahami Algoritma Machine Learning untuk Data Science',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium
+                                    ?.copyWith(fontWeight: FontWeight.w600),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 5),
+                              child: Text(
+                                'Oleh: GDSC Universitas Muhammadiyah Surakarta',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelMedium
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .outline,
+                                        fontSize: 8),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            )
                           ],
                         ),
-                      ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
 
-              //Populer List Event scroll horizontal
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Text(
-                  'Populer Event',
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(fontWeight: FontWeight.w600, fontSize: 18),
+                //Seminar list Event scroll horizontal
+                const SizedBox(height: 8),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Text(
+                    'Seminar Event',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
                 ),
-              ),
-              InkWell(
-                onTap: () => Get.toNamed(Routes.EVENT_DETAIL),
-                child: Container(
+                Container(
                   width: 155,
                   margin: const EdgeInsets.symmetric(horizontal: 16),
                   clipBehavior: Clip.hardEdge,
@@ -181,97 +295,8 @@ class EventView extends GetView<EventController> {
                     ],
                   ),
                 ),
-              ),
-
-              //Seminar list Event scroll horizontal
-              const SizedBox(height: 8),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Text(
-                  'Seminar Event',
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(fontWeight: FontWeight.w600, fontSize: 18),
-                ),
-              ),
-              Container(
-                width: 155,
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      width: 0.8,
-                      color: Theme.of(context).colorScheme.outlineVariant,
-                      style: BorderStyle.solid),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      "assets/img/event 1.jpeg",
-                      fit: BoxFit.cover,
-                    ),
-                    Wrap(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 5),
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 5),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  width: 0.8,
-                                  color: Theme.of(context).colorScheme.outline),
-                              borderRadius: BorderRadius.circular(5)),
-                          child: Text(
-                            'Seminar',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge
-                                ?.copyWith(
-                                    fontWeight: FontWeight.w600,
-                                    color:
-                                        Theme.of(context).colorScheme.outline,
-                                    fontSize: 12),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 5, vertical: 5),
-                          child: Text(
-                            'DevCoach 155 : Data Science | Memahami Algoritma Machine Learning untuk Data Science',
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium
-                                ?.copyWith(fontWeight: FontWeight.w600),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 5, vertical: 5),
-                          child: Text(
-                            'Oleh: GDSC Universitas Muhammadiyah Surakarta',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelMedium
-                                ?.copyWith(
-                                    color:
-                                        Theme.of(context).colorScheme.outline,
-                                    fontSize: 8),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
