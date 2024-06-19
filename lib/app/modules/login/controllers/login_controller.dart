45720131437acab91e3e0ac6894db81d91092d10
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-06-19 21:57:00 +0700

    finishing login & registerr backend

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,23 +1,69 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   //TODO: Implement LoginController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  var isPasswordObscure = true.obs;
+  var isLoading = false.obs;
+  String? errorMessage;
+
+  final auth = FirebaseAuth.instance;
+
+  void onLogin() async {
+    try {} on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        await auth.signInWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text);
+      } else if (e.code == 'wrong-password') {
+        if (e.code == 'user-not-found') {
+          errorMessage = 'No user found for that email.';
+        } else if (e.code == 'wrong-password') {
+          errorMessage = 'Wrong password provided for that user.';
+        }
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    }
+    isLoading.value = false;
+    Get.defaultDialog(
+      title: errorMessage == null ? "Succes" : "Error",
+      middleText: errorMessage == null
+          ? "Success to login on X buddy enjoy"
+          : "Failed to login because : $errorMessage",
+      onConfirm: () {
+        Get.back();
+        Get.back();
+      },
+    );
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void onChangeVisibilitPassword() {
+    isPasswordObscure.value = !isPasswordObscure.value;
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  String? onEmailValidation(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Password can't be empty";
+    }
+    if (!GetUtils.isEmail(value)) {
+      return "Please input the correct email";
+    }
+    return null;
   }
 
-  void increment() => count.value++;
+  String? onPasswordValidation(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Password can't be empty";
+    }
+    if (value.length < 8) {
+      return "Password can't be less than 8 char";
+    }
+    return null;
+  }
 }
--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-import '../../../routes/app_pages.dart';
 import '../controllers/register_controller.dart';
 
 class RegisterView extends GetView<RegisterController> {
@@ -14,107 +13,146 @@ class RegisterView extends GetView<RegisterController> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
-      body: Container(
-        width: Get.width,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("assets/img/illustration_register.png"),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Join us in XBuddy",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge
-                        ?.copyWith(fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "Create your account",
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text("Name"),
-                      border: OutlineInputBorder(),
-                      hintText: "Insert Your Name",
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text("Email"),
-                      border: OutlineInputBorder(),
-                      hintText: "Insert Your Email",
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text("Password"),
-                      border: OutlineInputBorder(),
-                      hintText: "Insert Your Password",
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text("Confirm Password"),
-                      border: OutlineInputBorder(),
-                      hintText: "Repeat Your Password",
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  SizedBox(
-                    width: Get.width,
-                    child: FilledButton(
-                      onPressed: () {},
-                      child: const Text("Register"),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text("Already have an account?"),
-                      TextButton(
-                        onPressed: () {
-                          Get.back();
-                        },
-                        child: Text(
-                          "Login",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(
-                                color: Theme.of(context).colorScheme.primary,
+      body: Obx(
+        () => controller.isLoading.value
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                width: Get.width,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: controller.formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/img/illustration_register.png"),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Join us in XBuddy",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "Create your account",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          TextFormField(
+                            controller: controller.nameController,
+                            validator: controller.onNameValidation,
+                            decoration: const InputDecoration(
+                              label: Text("Name"),
+                              border: OutlineInputBorder(),
+                              hintText: "Insert Your Name",
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: controller.emailController,
+                            validator: controller.onEmailValidation,
+                            decoration: const InputDecoration(
+                              label: Text("Email"),
+                              border: OutlineInputBorder(),
+                              hintText: "Insert Your Email",
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Obx(
+                            () => TextFormField(
+                              controller: controller.passwordController,
+                              validator: controller.onPasswordValidation,
+                              obscureText: controller.isPasswordObscure.value,
+                              decoration: InputDecoration(
+                                label: const Text("Password"),
+                                border: const OutlineInputBorder(),
+                                hintText: "Insert Your Password",
+                                suffixIcon: IconButton(
+                                  icon: controller.isPasswordObscure.value
+                                      ? const Icon(Icons.visibility)
+                                      : const Icon(Icons.visibility_off),
+                                  onPressed:
+                                      controller.onChangeVisibilitPassword,
+                                ),
                               ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Obx(
+                            () => TextFormField(
+                              controller: controller.confirmPasswordController,
+                              validator: controller.onConfirmPasswordValidation,
+                              obscureText:
+                                  controller.isConfirmPasswordObscure.value,
+                              decoration: InputDecoration(
+                                label: const Text("Confirm Password"),
+                                border: const OutlineInputBorder(),
+                                hintText: "Repeat Your Password",
+                                suffixIcon: IconButton(
+                                  icon:
+                                      controller.isConfirmPasswordObscure.value
+                                          ? const Icon(Icons.visibility)
+                                          : const Icon(Icons.visibility_off),
+                                  onPressed: controller
+                                      .onChangeVisibilitConfirmPassword,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          SizedBox(
+                            width: Get.width,
+                            child: FilledButton(
+                              onPressed: controller.onRegister,
+                              child: const Text("Register"),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Text("Already have an account?"),
+                              TextButton(
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: Text(
+                                  "Login",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                      ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
       ),
     );
   }
