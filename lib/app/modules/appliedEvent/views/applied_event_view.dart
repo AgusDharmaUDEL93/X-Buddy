72b1a9e150d6ae8a79bd3db5856278c559ab1ac4
Siswadi24 <danaperdanaputra32@gmail.com>
2024-06-21 22:33:19 +0700

    feat: Implementation of logic on the AppliedEvent page to retrieve events applied by the user

--- lib/app/modules/appliedEvent/views/applied_event_view.dart
+++ lib/app/modules/appliedEvent/views/applied_event_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:get/get.dart';
 
+import '../../../data/model/event.dart';
 import '../../../widgets/card_event.dart';
 import '../controllers/applied_event_controller.dart';
 
@@ -20,23 +21,54 @@ class AppliedEventView extends GetView<AppliedEventController> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: MasonryGridView.builder(
-          gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
-          itemCount: 5,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          itemBuilder: (context, index) {
-            return CardEvent(
-              category: "",
-              title: '',
-              author: '',
-              image: "",
-              onTap: () {
-                // Get.toNamed(Routes.EVENT_DETAIL);
-              },
-            );
-          },
+        child: GetBuilder<AppliedEventController>(
+          builder: (_) => StreamBuilder<List<Event>>(
+              stream: controller.getAppliedEvent(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                      child: CircularProgressIndicator(
+                          color: Theme.of(context).colorScheme.primary));
+                }
+                if (snapshot.hasError) {
+                  print('Error: ${snapshot.error}');
+                  print('Snapshot has data: ${snapshot.data}');
+                  return Center(
+                    child: Text(snapshot.error.toString()),
+                  );
+                }
+                if (snapshot.hasData) {
+                  print('ini dijalankan');
+
+                  return MasonryGridView.builder(
+                    gridDelegate:
+                        const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemCount: snapshot.data?.length,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    itemBuilder: (_, index) {
+                      final event = snapshot.data![index];
+                      return CardEvent(
+                        category: event.category,
+                        title: event.title,
+                        author: event.authorName,
+                        image: event.image,
+                        onTap: () {
+                          // Get.toNamed(Routes.EDIT_EVENT,
+                          //     arguments: event.toJson(),
+                          //     parameters: {'event_id': event.eventId});
+                        },
+                      );
+                    },
+                  );
+                } else {
+                  print('No data available');
+                  return const Center(
+                    child: Text('No data available'),
+                  );
+                }
+              }),
         ),
       ),
     );
