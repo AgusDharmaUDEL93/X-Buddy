631cb0bdf8319fb48399fb3619727bee617a838a
Siswadi24 <danaperdanaputra32@gmail.com>
2024-06-21 22:33:19 +0700

    feat: Implementation of logic on the AppliedEvent page to retrieve events applied by the user

--- lib/app/modules/appliedEvent/controllers/applied_event_controller.dart
+++ lib/app/modules/appliedEvent/controllers/applied_event_controller.dart
@@ -1,23 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
+import '../../../data/model/event.dart';
+
 class AppliedEventController extends GetxController {
-  //TODO: Implement AppliedEventController
+  //Method untuk mengambil data event yang telah di daftar oleh user dan user itu sudah login
+  Stream<List<Event>> getAppliedEvent() {
+    User? currentUser = FirebaseAuth.instance.currentUser;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+    if (currentUser != null) {
+      return FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.uid)
+          .snapshots()
+          .asyncMap((doc) async {
+        List<dynamic> followedEvents = doc.data()?['followed_event'];
+        List<String> eventIds = followedEvents.cast<String>();
+
+        List<Event> events = [];
+        for (String eventId in eventIds) {
+          DocumentSnapshot<Map<String, dynamic>> eventSnapshot =
+              await FirebaseFirestore.instance
+                  .collection('events')
+                  .doc(eventId)
+                  .get();
+          if (eventSnapshot.exists) {
+            events.add(Event.fromJson(eventSnapshot.data()!));
+          }
+        }
+        return events;
+      });
+    } else {
+      return const Stream.empty();
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  // final count = 0.obs;
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+  // @override
+  // void onReady() {
+  //   super.onReady();
+  // }
 
-  void increment() => count.value++;
+  // @override
+  // void onClose() {
+  //   super.onClose();
+  // }
+
+  // void increment() => count.value++;
 }
