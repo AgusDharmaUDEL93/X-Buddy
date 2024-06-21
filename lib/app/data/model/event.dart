0922e925b4a4e9157bc6cd6cff8d21a0affdb725
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-06-21 15:49:45 +0700

    rebasing with main

--- lib/app/data/model/event.dart
+++ lib/app/data/model/event.dart
@@ -9,7 +9,6 @@ Event eventFromJson(String str) => Event.fromJson(json.decode(str));
 String eventToJson(Event data) => json.encode(data.toJson());
 
 class Event {
-<<<<<<< HEAD
     String eventId;
     String title;
     String image;
@@ -40,32 +39,6 @@ class Event {
         eventId: json["event_id"],
         title: json["title"],
         image: json["image"],
-=======
-  String title;
-  String category;
-  String authorUid;
-  String authorName;
-  String location;
-  String date;
-  String time;
-  int participant;
-  String description;
-
-  Event({
-    required this.title,
-    required this.category,
-    required this.authorUid,
-    required this.authorName,
-    required this.location,
-    required this.date,
-    required this.time,
-    required this.participant,
-    required this.description,
-  });
-
-  factory Event.fromJson(Map<String, dynamic> json) => Event(
-        title: json["title"],
->>>>>>> 53a4dec (fixing some model configuration)
         category: json["category"],
         authorUid: json["author_uid"],
         authorName: json["author_name"],
@@ -74,19 +47,12 @@ class Event {
         time: json["time"],
         participant: json["participant"],
         description: json["description"],
-<<<<<<< HEAD
     );
 
     Map<String, dynamic> toJson() => {
         "event_id": eventId,
         "title": title,
         "image": image,
-=======
-      );
-
-  Map<String, dynamic> toJson() => {
-        "title": title,
->>>>>>> 53a4dec (fixing some model configuration)
         "category": category,
         "author_uid": authorUid,
         "author_name": authorName,
@@ -95,9 +61,5 @@ class Event {
         "time": time,
         "participant": participant,
         "description": description,
-<<<<<<< HEAD
     };
-=======
-      };
->>>>>>> 53a4dec (fixing some model configuration)
 }
