15b18839d87fc6d32d940e3de745994463e80ce2
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-06-21 18:04:23 +0700

    finishing the profile page

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,26 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
-  //TODO: Implement ProfileController
-
-  final count = 0.obs;
   var isLogin = false.obs;
-  var isVerify = true.obs;
+  var isVerify = false.obs;
+
+  var isLoading = false.obs;
+  String? errorMessage;
+
+  var name = "".obs;
+  var email = "".obs;
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
+    onRefresh();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void onLogout() {
+    Get.defaultDialog(
+      title: "Logout",
+      middleText: "Are u sure want to logout?",
+      onConfirm: () async {
+        await auth.signOut();
+        onRefresh();
+        Get.back();
+      },
+      onCancel: () {
+        Get.back();
+      },
+    );
   }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+  void onRefresh() async {
+    isLoading.value = true;
 
-  void increment() => count.value++;
+    if (auth.currentUser == null) {
+      isLoading.value = false;
+      isLogin.value = false;
+      return;
+    }
+
+    isLogin.value = true;
+    email.value = auth.currentUser?.email ?? "";
+
+    await firestore
+        .collection("users")
+        .doc(auth.currentUser?.uid)
+        .get()
+        .then((DocumentSnapshot document) {
+      if (document.exists) {
+        isVerify.value = document["isVerify"];
+        name.value = document["name"];
+      } else {
+        errorMessage = 'Document does not exist on the database';
+      }
+    });
+
+    isLoading.value = false;
+  }
 }
