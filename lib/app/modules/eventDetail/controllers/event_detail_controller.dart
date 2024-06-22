2b85a56fdaa95c26b3ebcd0b53a2c4eb8c752be7
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-06-22 11:04:47 +0700

    finishing the detail page

--- lib/app/modules/eventDetail/controllers/event_detail_controller.dart
+++ lib/app/modules/eventDetail/controllers/event_detail_controller.dart
@@ -1,23 +1,112 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class EventDetailController extends GetxController {
-  //TODO: Implement EventDetailController
+import '../../../data/model/event.dart';
+import '../../../data/model/user.dart' as Usr;
 
-  final count = 0.obs;
+class EventDetailController extends GetxController {
+  var isLoading = false.obs;
+  String? errorMessage;
+
+  late String eventId;
+  var isJoined = false.obs;
+
+  final firestore = FirebaseFirestore.instance;
+  final auth = FirebaseAuth.instance;
+
   @override
   void onInit() {
     super.onInit();
+
+    isLoading.value = true;
+
+    eventId = Get.parameters["id"].toString();
+    checkIsJoined();
+
+    isLoading.value = false;
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Stream<Event> getEventDetail() {
+    return firestore
+        .collection('events')
+        .doc(eventId)
+        .snapshots()
+        .map((snapshot) => Event.fromJson(snapshot.data()!));
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void onAddEvent() async {
+    if (auth.currentUser == null) {
+      Get.defaultDialog(
+        title: "Error",
+        middleText: "Can't join the event, please login first!",
+        onConfirm: () {
+          Get.back();
+        },
+      );
+      return;
+    }
+
+    isLoading.value = true;
+
+    if (isJoined.value) {
+      await firestore
+          .collection("events")
+          .doc(eventId)
+          .get()
+          .then((DocumentSnapshot document) async {
+        if (document.exists) {
+          await firestore.collection("events").doc(eventId).update({
+            "participant":
+                (document["participant"] > 0 ? document["participant"] : 1) - 1
+          });
+          await firestore
+              .collection("users")
+              .doc(auth.currentUser?.uid)
+              .update({
+            "followed_event": FieldValue.arrayRemove([eventId])
+          });
+        } else {
+          errorMessage = 'Document does not exist on the database';
+        }
+      });
+    } else {
+      await firestore
+          .collection("events")
+          .doc(eventId)
+          .get()
+          .then((DocumentSnapshot document) async {
+        if (document.exists) {
+          await firestore
+              .collection("events")
+              .doc(eventId)
+              .update({"participant": document["participant"] + 1});
+          await firestore
+              .collection("users")
+              .doc(auth.currentUser?.uid)
+              .update({
+            "followed_event": FieldValue.arrayUnion([eventId])
+          });
+        } else {
+          errorMessage = 'Document does not exist on the database';
+        }
+      });
+    }
+    checkIsJoined();
+    isLoading.value = false;
   }
 
-  void increment() => count.value++;
+  void checkIsJoined() async {
+    await firestore
+        .collection("users")
+        .doc(auth.currentUser?.uid)
+        .get()
+        .then((DocumentSnapshot document) {
+      if (document.exists) {
+        isJoined.value = document["followed_event"].contains(eventId);
+      } else {
+        errorMessage = 'Document does not exist on the database';
+      }
+    });
+  }
 }
